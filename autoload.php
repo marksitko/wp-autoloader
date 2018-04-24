da0e252f9f8346a43d37a878fe2563156d87ac7e
Mark Sitko <mark@sitko-designing.de>
2018-04-24 13:24:29 +0200

    changed function order

--- autoload.php
+++ autoload.php
@@ -29,6 +29,25 @@ class ClassLoader
      */
     private static $instance = null;
 
+    /**
+     *
+     * @param array $namespaces
+     */
+    public function __construct(array $namespaces)
+    {
+        $this->addNamespaces($namespaces);
+    }
+
+    /**
+     * @param string $class
+     */
+    public function __invoke($class)
+    {
+        if ($file = $this->findFile($class)) {
+            include $file;
+            return true;
+        }
+    }
 
     /**
      * Initialize autoload
@@ -67,55 +86,6 @@ class ClassLoader
     }
 
     /**
-     * Construct
-     *
-     * @param array $namespaces
-     */
-    public function __construct(array $namespaces)
-    {
-        $this->addNamespaces($namespaces);
-    }
-
-    /**
-     * @param string $class
-     * @return boolean|string
-     */
-    protected function findFile($class)
-    {
-        $first = substr($class, 0, 1);
-
-        if (!isset($this->namespaces[$first])) {
-            return false;
-        }
-
-        foreach ($this->namespaces[$first] as $prefix => $paths) {
-            if (strpos($class, $prefix) === 0) {
-                $file = substr($class, strlen($prefix));
-                $file = str_replace('\\', DIRECTORY_SEPARATOR, $file) . '.php';
-
-                foreach ($paths as $path) {
-                    if (file_exists($path. DIRECTORY_SEPARATOR . $file)) {
-                        return $path. DIRECTORY_SEPARATOR . $file;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @param string $class
-     */
-    public function __invoke($class)
-    {
-        if ($file = $this->findFile($class)) {
-            include $file;
-            return true;
-        }
-    }
-
-    /**
      * @return self
      */
     public function addNamespaces(array $namespaces)
@@ -150,8 +120,34 @@ class ClassLoader
             $this->namespaces[$first][$prefix][] = $path;
         }
     }
+
+    /**
+     * @param string $class
+     * @return boolean|string
+     */
+    protected function findFile($class)
+    {
+        $first = substr($class, 0, 1);
+
+        if (!isset($this->namespaces[$first])) {
+            return false;
+        }
+
+        foreach ($this->namespaces[$first] as $prefix => $paths) {
+            if (strpos($class, $prefix) === 0) {
+                $file = substr($class, strlen($prefix));
+                $file = str_replace('\\', DIRECTORY_SEPARATOR, $file) . '.php';
+
+                foreach ($paths as $path) {
+                    if (file_exists($path . DIRECTORY_SEPARATOR . $file)) {
+                        return $path . DIRECTORY_SEPARATOR . $file;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
 }
 
-
-// Init autoloader
 ClassLoader::init();
